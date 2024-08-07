4be6bef8c3efd760501b91761d2cc64ffe92bc5c
Sabina <sabinagamidova721@gmail.com>
2024-08-07 16:03:18 +0300

    Add UsersApi module

---
+++ UsersApi/src/test/java/com/movieapp/users/api/UsersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieapp.users.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
