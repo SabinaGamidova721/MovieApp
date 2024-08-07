1d9fb57a59924c28b43bb49daef6ad55ed06246e
Sabina <sabinagamidova721@gmail.com>
2024-08-07 16:56:51 +0300

    Change configuration for Gateway => appropriate UserApi working => now have an ability to send http-requests through gateway to user module

---
+++ UsersApi/src/main/java/com/movieapp/users/api/controllers/UsersController.java
@@ -0,0 +1,18 @@
+package com.movieapp.users.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @Autowired
+    private Environment env;
+
+    @GetMapping("/status/check")
+    public String status() {
+        return "Working on port: " + env.getProperty("local.server.port");
+    }
+}
